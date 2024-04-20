c94e25eaaad88fa6ae0e420f88c0991bd44cea83
dimashbalabek <dimashbalabek0@gmail.com>
2024-04-20 14:08:49 +0600

    finished quiz

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,220 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      debugShowCheckedModeBanner:
+      false,
+      home: Scaffold(
+        backgroundColor: Color(int.parse('0xFFBFDAF5')),
+        appBar: _appBar(),
+        body: _buildBody(),
+
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+AppBar _appBar() {
+  return AppBar(
+    backgroundColor: Colors.blue[800],
+    title: Text("Weather Forecast", style: TextStyle(color: Colors.white),),
+    centerTitle: true,
+  );
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+Widget _buildBody() {
+  return SingleChildScrollView(
+    child: Padding(
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          _search(),
+          const SizedBox(height: 20,),
+          _mainText(),
+          const SizedBox(height: 20,),
+          _weather(),
+          const SizedBox(height: 20,),
+          _statistics(),
+          const SizedBox(height: 20,),
+          _sevenDayWeather()
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    ),
+  );
+}
+
+
+
+Widget _search() {
+  return TextField(
+    decoration: InputDecoration(
+        prefixIcon: Icon(Icons.search, color: Colors.blue,),
+        filled: true,
+        fillColor: Colors.white,
+        hintText: "Enter City Name",
+        hintStyle: TextStyle(color: Colors.black45),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: Colors.transparent, width: 0,),
+
+
+        )
+    ),
+  );
+}
+
+Column _mainText() {
+  return Column(
+    children: [
+      Text(
+        "Pushkin 154, Taraz",
+        style: TextStyle(
+          fontSize:30,
+          color: Colors.blue[1000],
+          fontWeight: FontWeight.w400
+        ),),
+      const Text(
+        "Friday, April 19, 2024",
+        style: TextStyle(
+          fontSize: 18,
+          color: Color(0xFF004080)
+        ),
+      )
+    ],
+  );
+}
+
+Row _weather() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Column(
+        children: [Icon(Icons.wb_sunny_rounded, size: 80, color: Color(int.parse('0xFFF7F9A6')),)],
+      ),
+      const SizedBox(width: 15,),
+      const Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("14°F", style: TextStyle(fontSize: 45, color: Colors.white),),
+          Text("LIGHT SHOW", style: TextStyle(fontSize: 15, color: Colors.grey),)
+        ],
+      )
+    ],
+  );
+}
+
+Row _statistics() {
+  var array = ["km/hr", "%", "%"];
+  var array2 = ["5", "3", "20"];
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children:
+      List.generate(3, (int index) => Column(
+        children: [
+          const Icon(Icons.ac_unit,size: 45, color: Color(0xFF004080),),
+          Text("${array2[index]}",
+          style: const TextStyle(
+            fontSize:26,
+            color: Colors.white
+          ),),
+        Text("${array[index]}",
+        style: const TextStyle(
+          color: Color(0xFF004080),
+          fontSize: 18
+        ),)
+        ],
+      ))
+  );
+}
+
+Widget _sevenDayWeather() {
+  return Column(
+    children: [
+      const Text("7 DAY WEATHER FORECAST", style: TextStyle(fontSize: 22, color: Colors.white, fontWeight: FontWeight.w700),),
+      const SizedBox(height: 10,),
+      Container(
+        height: 100,
+        width: 400,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: [
+            Container(
+              width: 150,
+              height: 75,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12), // Set the border radius here
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Friday", style: TextStyle(fontSize: 22),),
+                  Row(
+                    mainAxisAlignment:  MainAxisAlignment.center,
+                    children: [
+                      Text("6°F", style: TextStyle(fontSize: 16, color: Color(int.parse('0xFF5F89A3'))),),
+                      Icon(Icons.wb_sunny_rounded, color: Colors.yellowAccent,)
+                    ],
+                  )
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+
+            SizedBox(width: 14,),
+            Container(
+              width: 150,
+              height: 75,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12), // Set the border radius here
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Saturday", style: TextStyle(fontSize: 22),),
+                  Row(
+                    mainAxisAlignment:  MainAxisAlignment.center,
+                    children: [
+                      Text("5°F", style: TextStyle(fontSize: 16, color: Color(int.parse('0xFF5F89A3'))),),
+                      Icon(Icons.cloud, color: Colors.grey,)
+                    ],
+                  )
+                ],
+              ),
             ),
+            SizedBox(width: 14,),
+            Container(
+              width: 150,
+              height: 75,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12), // Set the border radius here
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Sunday", style: TextStyle(fontSize: 22),),
+                  Row(
+                    mainAxisAlignment:  MainAxisAlignment.center,
+                    children: [
+                      Text("7°F", style: TextStyle(fontSize: 16, color: Color(int.parse('0xFF5F89A3'))),),
+                      Icon(Icons.wb_sunny_rounded, color: Colors.yellowAccent,)
+                    ],
+                  )
+                ],
+              ),
+            ),
+
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+      )
+    ],
+  );
 }
